ece603009902105372f69a508718302e9b595e49
Todd Bashor <todd@startersacademy.com>
2016-03-09 15:40:24 -0800

    Create chance instance instead of requiring the consumer to do it.

--- plugin.js
+++ plugin.js
@@ -4,7 +4,7 @@ module.exports = function(options, imports, register) {
   var log = imports.debug('plugins:chance');
   log('start');
 
-  var chance = require('chance');
+  var chance = require('chance').Chance();
 
   var api = {
     random: chance
